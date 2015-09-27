4b57f5a8b79a14f0ec8e32c08c193bcb0808baeb
planets09 <rena.tanrx@gmail.com>
2015-09-27 12:39:07 -0700

    all project files from hello web app

---
+++ hellowebapp/collection/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	#defining the variable
+	number = 6
+	thing = "Thing name"
+	#this is your new view
+	return render(request, 'index.html',{'number': number, 'thing': thing,})
+
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\planets09\projects\hellowebapp\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
